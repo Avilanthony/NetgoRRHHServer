c2ba15816b7cc11983d76c7f32d001efe5bdaec6
Anthony Avila <anthony.avila.aa2@gmail.com>
2023-12-08 12:25:50 -0600

    conexion a la rds de aws

--- src/server/models/modulo_seguridad/rol.js
+++ src/server/models/modulo_seguridad/rol.js
@@ -1,16 +1,17 @@
-const {DataTypes} = require('sequelize');
+const { DataTypes } = require('sequelize');
 const rrhhNetgo = require('../../database/database');
 
 const ROLES = rrhhNetgo.define('tbl_ms_rol', {
-    ID_ROL: {
-        type: DataTypes.INTEGER,
-        primaryKey: true
-    },
-    ROL: {
-        type: DataTypes.STRING
-    },
-},{
-    tableName: 'tbl_ms_rol', timestamps: false
-}); //DECLARANDO EL OBJETO
+  ID_ROL: {
+    type: DataTypes.INTEGER,
+    primaryKey: true
+  },
+  ROL: {
+    type: DataTypes.STRING
+  },
+}, {
+  tableName: 'tbl_ms_rol',
+  timestamps: false
+});
 
 module.exports = ROLES;
