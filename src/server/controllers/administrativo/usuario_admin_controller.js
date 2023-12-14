475b5208da11a89d5023718711e57728b47e2cb6
DoniG09 <81458410+DoniG09@users.noreply.github.com>
2023-12-14 02:05:30 -0600

    Cambios en api para traer datos de usuarios a gestionar

--- src/server/controllers/administrativo/usuario_admin_controller.js
+++ src/server/controllers/administrativo/usuario_admin_controller.js
@@ -3,7 +3,7 @@ const { request, response } = require("express");
 const ViewUserAdminEdit = require("../../models/modulo_seguridad/views/usuario_admin_edit_view");
 
 
-const getUsuarioEditAdmin = async (req = request, res = response) => {
+/* const getUsuarioEditAdmin = async (req = request, res = response) => {
     const {id_usuario} = req.params;
 
     try {
@@ -20,6 +20,11 @@ const getUsuarioEditAdmin = async (req = request, res = response) => {
         }
 
         res.json({usuario});
+        return res.status(200).json({
+            ok: true,
+            status: true,
+            msg: "¡Usuario localizado con éxito!"
+        });
     } catch (error) {
         console.error(error);
 
@@ -27,6 +32,42 @@ const getUsuarioEditAdmin = async (req = request, res = response) => {
             msg: error.message
         });
     }
+}; */
+
+const getUsuarioEditAdmin = async (req = request, res = response) => {
+    const { id_usuario } = req.params;
+
+    try {
+        console.log('ID del usuario recibido:', id_usuario);
+
+        const usuario = await ViewUserAdminEdit.findByPk(id_usuario);
+
+        if (!usuario) {
+            console.log('Usuario no encontrado');
+            return res.status(404).json({
+                ok: false,
+                msg: "No existe el usuario"
+            });
+        }
+
+        // Envía la respuesta al cliente
+        res.status(200).json({
+            ok: true,
+            status: true,
+            msg: "¡Usuario localizado con éxito!",
+            usuario
+        });
+    } catch (error) {
+        console.error(error);
+
+        res.status(500).json({
+            msg: error.message
+        });
+    }
+};
+
+module.exports = {
+    getUsuarioEditAdmin
 };
 
 
