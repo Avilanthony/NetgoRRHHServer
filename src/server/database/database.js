417c7618f6b4a80466a93be6ed85d09decb42a07
DoniG09 <81458410+DoniG09@users.noreply.github.com>
2024-01-21 20:37:02 -0600

    Server v1 ~ Completada

--- src/server/database/database.js
+++ src/server/database/database.js
@@ -1,19 +1,16 @@
-const { Sequelize } = require('sequelize');
+const { Sequelize } = require("sequelize");
 
-const rrhhNetgo = new Sequelize({
-  host: 'rrhhnetgo-pruebas.cvhq4rmcypqh.us-east-2.rds.amazonaws.com',
-  database: 'rrhh_netgo',
-  username: 'admin',
-  password: 'rootNetgoServer',
-  dialect: 'mysql',
+const {
+    SCHEMA ,
+    MYSQL_HOST ,
+    SQL_USER ,
+    SQL_PASSWORD ,
+}=process.env;
+
+// Option 3: Passing parameters separately (other dialects)
+const rrhhNetgo = new Sequelize(SCHEMA, SQL_USER, SQL_PASSWORD, {
+  host: MYSQL_HOST,
+  dialect: 'mysql'
 });
-
-rrhhNetgo.authenticate()
-  .then(() => {
-    console.log('Conexión a la base de datos establecida correctamente.');
-  })
-  .catch(err => {
-    console.error('Error al conectar a la base de datos:', err);
-  });
 
 module.exports = rrhhNetgo;
